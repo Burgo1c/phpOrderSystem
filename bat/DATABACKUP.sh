df6c6bf069c1afc85d4dd2cc4988a41f96e4db4d
Burgo1c <72175382+Burgo1c@users.noreply.github.com>
2023-12-05 14:40:58 +0900

    Update DATABACKUP.sh

--- bat/DATABACKUP.sh
+++ bat/DATABACKUP.sh
@@ -1,5 +1,5 @@
 # BACKUP DATABASE
-PGPASSWORD='UedaUskk' pg_dump -U ueda uedaorder > /ADD_DISK/DB/Backup/$(date +%Y%m%d)_uedaDB.sql
+PGPASSWORD='password' pg_dump -U user public > /ADD_DISK/DB/Backup/$(date +%Y%m%d)_DB.sql
 
 # Delete backups older than 7 days
-find /ADD_DISK/DB/Backup -name "*_uedaDB.sql" -mtime +6 -exec rm {} \;
+find /ADD_DISK/DB/Backup -name "*_DB.sql" -mtime +6 -exec rm {} \;
